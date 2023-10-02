071626734d83e66e0195467a2df4f2d965f2ccda
Andrew Donovan <a.o.donovan.music@gmail.com>
2023-10-02 00:45:45 -0400

    disable csrf again

--- app.py
+++ app.py
@@ -79,6 +79,7 @@ def do_logout():
 
 
 @app.route('/signup', methods=["GET", "POST"])
+@csrf.exempt
 def signup():
     """Handle user signup.
 
@@ -117,6 +118,7 @@ def signup():
         return render_template('signup.html', form=form)
     
 @app.route('/login', methods=["GET", "POST"])
+@csrf.exempt
 def login():
     """Handle user login."""
     print('login')
